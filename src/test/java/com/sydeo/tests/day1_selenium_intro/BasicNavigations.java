4ce919a65248d927c0435602c9e092237e1b5c6d
Bahar Taskin <cakirbhr7@gmail.com>
2022-12-20 20:08:07 +0300

    updated day 1

--- src/test/java/com/sydeo/tests/day1_selenium_intro/BasicNavigations.java
+++ src/test/java/com/sydeo/tests/day1_selenium_intro/BasicNavigations.java
@@ -17,6 +17,14 @@ public class BasicNavigations {
         // 3- go to tesla.com
         driver.get("https://www.tesla.com");
 
+        // get the title of the page
+        String currentTitle = driver.getTitle();
+        System.out.println("currentTitle = " + currentTitle);
+
+        // get the current URL
+        String currentURL = driver.getCurrentUrl();
+        System.out.println("currentURL = " + currentURL);
+
         // stop code execution for 3 seconds
         Thread.sleep(3000);
 
@@ -40,6 +48,28 @@ public class BasicNavigations {
 
         // use navigate().to():
         driver.navigate().to("https://www.google.com");
+
+        // get the current title of the page after getting google page
+       // System.out.println(driver.getTitle());
+        currentTitle = driver.getTitle();
+        System.out.println("currentTitle = " + currentTitle);
+
+        // get the current URL
+        currentURL = driver.getCurrentUrl();
+        System.out.println("currentURL = " + currentURL);
+
+        // maximize the browser size
+        driver.manage().window().maximize();
+
+       // driver.manage().window().fullscreen();
+
+
+        // this will close the currently opened window
+        driver.close();
+
+        // this will close all the opened windows
+        driver.quit();
+
 
 
     }
---
+++ src/test/java/com/sydeo/tests/day1_selenium_intro/Task1_YahooTitleVerification.java
@@ -0,0 +1,41 @@
+package com.sydeo.tests.day1_selenium_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Task1_YahooTitleVerification {
+
+    public static void main(String[] args) {
+
+        //TC #1: Yahoo Title Verification
+        //1. Open Chrome browser
+
+        //  set up the browser driver
+        WebDriverManager.chromedriver().setup();
+
+        //  create instance of the Selenium  Webdriver
+        WebDriver driver = new ChromeDriver(); // opens empty browser
+
+        driver.manage().window().maximize(); // to make the page fullscreen
+
+        //2. Go to https://www.yahoo.com
+        driver.get("https://www.yahoo.com");
+
+        //3. Verify title:
+
+        String expectedTitle = "Yahoo | Mail, Weather, Search, Politics, News, Finance, Sports & Videos";
+
+        String actualTitle = driver.getTitle();
+
+        if (actualTitle.equals(expectedTitle)){
+            System.out.println("Title is as expected. Verification PASSED!");
+        }else{
+            System.out.println("Title is NOT as expected. Verification FAILED!");
+        }
+
+        //Expected: Yahoo | Mail, Weather, Search, Politics, News, Finance, Sports & Videos
+
+    }
+
+}
