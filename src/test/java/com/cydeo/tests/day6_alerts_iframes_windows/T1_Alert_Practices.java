33f0c8c8725fb738eb929310ca68471d7ab4a163
Bahar Taskin <cakirbhr7@gmail.com>
2023-01-11 22:34:43 +0300

    updated day 6

---
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T1_Alert_Practices.java
@@ -0,0 +1,38 @@
+package com.cydeo.tests.day6_alerts_iframes_windows;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.*;
+import org.testng.*;
+import org.testng.annotations.*;
+
+import java.util.concurrent.*;
+public class T1_Alert_Practices {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod() {
+        //1. Open browser
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //2. Go to website: https://practice.cydeo.com/javascript_alerts
+        driver.get("https://practice.cydeo.com/javascript_alerts");
+    }
+
+    @Test
+    public void alert_test1() throws InterruptedException {
+        //3. Click to “Click for JS Alert” button
+        WebElement informationAlertButton = driver.findElement(By.xpath("//button[.='Click for JS Alert']"));
+
+
+        informationAlertButton.click();
+        Thread.sleep(1000);
+
+        //To be able to click to Alert OK button we need to switch driver's focus to Alert itself.
+        Alert alert = driver.switchTo().alert();
+
+
+    }
+}
